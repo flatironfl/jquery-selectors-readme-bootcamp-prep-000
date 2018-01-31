8bcff03cfec7304ffe442eea4d4b17a49e264663
First Last <flatironcamp.tc99@mamber.net>
2018-01-31 04:18:43 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,3 +3,23 @@
 function paragraphSelector() {
   return $('p');
 }
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('div .pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
